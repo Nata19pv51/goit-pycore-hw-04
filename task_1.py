1dd646051e1d25a4b09181fc4962c1698dca540e
Natalya <nidnatasha@gmail.com>
2025-10-15 15:26:21 +0300

    Task 1 create

---
+++ task_1.py
@@ -0,0 +1,29 @@
+# fh = open('text_task_1.txt', 'w+')
+# text_written = fh.write("""Alex Korp,3000
+# Nikita Borisenko,2000
+# Sitarama Raju,1000""")
+# print(text_written)
+# fh.close()
+
+
+def total_salary(path):
+    salary_list = []
+    with open(path, 'r', encoding='utf-8') as file:
+        while True:
+            line = file.readline()
+            line = line.strip()
+            line_list = line.split(',')
+            
+            if not line:
+                break
+            
+            salary_list.append(int(line_list[1]))
+    
+    total_salary = sum(salary_list)
+    average_salary = sum(salary_list) / len(salary_list)
+    salary_tuple = (total_salary, average_salary)
+    
+    return salary_tuple
+    
+total, average = total_salary("text_task_1.txt")
+print(f"Загальна сума заробітної плати: {total}, Середня заробітна плата: {average}")

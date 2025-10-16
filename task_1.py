fde414d15f99f278b47c75bbd0745125487bc031
Natalya <nidnatasha@gmail.com>
2025-10-16 11:44:26 +0300

    Add exception to the task 1

--- task_1.py
+++ task_1.py
@@ -8,22 +8,28 @@
 
 def total_salary(path):
     salary_list = []
+    
     with open(path, 'r', encoding='utf-8') as file:
         while True:
             line = file.readline()
             line = line.strip()
             line_list = line.split(',')
-            
+                
             if not line:
                 break
-            
+                
             salary_list.append(int(line_list[1]))
-    
+                
     total_salary = sum(salary_list)
     average_salary = sum(salary_list) / len(salary_list)
     salary_tuple = (total_salary, average_salary)
     
     return salary_tuple
     
-total, average = total_salary("text_task_1.txt")
-print(f"Загальна сума заробітної плати: {total}, Середня заробітна плата: {average}")
+try:
+    total, average = total_salary("text_task_1.txt")
+except FileNotFoundError:
+        # Handle the case where the file is not found
+        print("Error: The specified file was not found.")    
+else:       
+    print(f"Загальна сума заробітної плати: {total}, Середня заробітна плата: {average}")

43d1b5e124395fb104fbf17a32b4aabc55dd4a68
Natalya <nidnatasha@gmail.com>
2025-10-22 14:27:15 +0300

    Add gitignore, requirements files, fix task1, task3

--- task_1.py
+++ task_1.py
@@ -8,27 +8,34 @@
 
 def total_salary(path):
     salary_list = []
-    
-    with open(path, 'r', encoding='utf-8') as file:
-        while True:
-            line = file.readline()
-            line = line.strip()
-            line_list = line.split(',')
+        
+    try:
+        with open(path, 'r', encoding='utf-8') as file:
+            while True:
+                line = file.readline()
+                line = line.strip()
+                line_list = line.split(',')
+                    
+                if not line:
+                    break 
                 
-            if not line:
-                break
+                salary_list.append(float(line_list[1]))
                 
-            salary_list.append(int(line_list[1]))
-                
-    total_salary = sum(salary_list)
-    average_salary = sum(salary_list) / len(salary_list)
-    salary_tuple = (total_salary, average_salary)
+            total_salary = sum(salary_list)   
+            average_salary = sum(salary_list) / len(salary_list)
     
-    return salary_tuple
+    except FileNotFoundError:
+        # Handle the case where the file is not found
+        print("Error: The specified file was not found.")  
     
-try:
+    except ZeroDivisionError:
+        print("Error: Cannot divide by zero.")
+        return [0, 0]
+    
+    else: 
+        salary_tuple = (total_salary, average_salary)
+        return salary_tuple
+
+if __name__ == "__main__":
     total, average = total_salary("text_task_1.txt")
     print(f"Загальна сума заробітної плати: {total}, Середня заробітна плата: {average}")
-except FileNotFoundError:
-    # Handle the case where the file is not found
-    print("Error: The specified file was not found.")    
--- task_3/task_3.py
+++ task_3/task_3.py
@@ -1,7 +1,9 @@
 from pathlib import Path
 from colorama import Fore
+import sys
 
 def iterate_folder(path, prefix=""): 
+    
     # Виведення переліку всіх файлів та піддиректорій
     for path in path.iterdir():
         space = "   "
@@ -11,15 +13,22 @@ def iterate_folder(path, prefix=""):
             print(f"{prefix + space}{Fore.GREEN}{path.name}")
         
         if path.is_dir():   
-            # print(f"\t{Fore.BLUE}{path.name}")
             iterate_folder(path, prefix + space)
 
-try:
-    file = Path("pictures") 
-    print(f"{Fore.BLUE}{file.name}/")
-    iterate_folder(file)
-except FileNotFoundError:
-    # Handle the case where the file is not found
-    print("Error: The specified file was not found.")   
-except NotADirectoryError:
-    print("Error: The path provided is not a directory.")
+def main():
+    try:
+        file = sys.argv[1]
+        path_file = Path(file)
+        print(f"{Fore.BLUE}{path_file.name}/")
+        iterate_folder(path_file)
+        
+    except FileNotFoundError:
+        # Handle the case where the file is not found
+        print("Error: The specified file was not found.")   
+    except NotADirectoryError:
+        print("Error: The path provided is not a directory.")
+
+if __name__ == "__main__":
+    main()
+    
+        
